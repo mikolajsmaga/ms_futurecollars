3afd2d2d2dec478925b7a5dea473919e9c651dbb
mikolajsmaga <miko09@poczta.onet.pl>
2025-05-12 20:38:51 +0200

    zmiana if else na match case

--- Projekty/Python_5/ksiegowosc_magazyn.py
+++ Projekty/Python_5/ksiegowosc_magazyn.py
@@ -1,24 +1,53 @@
-from ctypes.macholib.dyld import DEFAULT_LIBRARY_FALLBACK
 
-konto = 0
+print("Witaj w programie księgowym! Zaczynamy?")
+
+konto = 0.0
 operacje_finansowe = []
+komenda = ["saldo", "sprzedaż", "zakup", "konto", "lista", "magazyn", "przegląd", "koniec"]
 
 while True:
     print("Dostępne komendy:")
-    print("saldo\nsprzedaż\nzakup\nkonto\nlista\nmagazyn\nprzegląd\nkoniec")
+    print("1. saldo","\n2. sprzedaż","\n3. zakup","\n4. konto","\n5. lista","\n6. magazyn","\n7. przegląd","\n8. koniec")
     komenda = input("Wprowadź komendę: ").strip().lower()
     if komenda == "koniec":
         print("Zakończ działanie programu!")
         break
 
-elif komenda == "saldo":
-    try:
-        kwota = float(input("Wprowadź kwotę (może być ujemna): "))
-        konto = kwota + 1
-        operacje_finansowe.append(("saldo", kwota))
-    except ValueError:
-        print("Błąd! Wprowadź poprawną wartość")
-    elif komenda == "konto":
-        print(f"Stan konta: {konto} zł")
-        else:
-            print("Nieznana komenda, spróbuj ponownie!")
+    match komenda:
+        case "saldo":
+            kwota = float(input("Wprowadź kwotę (może być ujemna): "))
+            konto += kwota
+            operacje_finansowe.append(("saldo", kwota))
+            if kwota < 0:
+                print("Masz debet na koncie!")
+
+
+
+
+print(f"Twój stan konta: {konto}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

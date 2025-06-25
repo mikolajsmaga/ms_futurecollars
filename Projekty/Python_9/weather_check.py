93a5a723706859372885ac37038d3b86f1994ce7
mikolajsmaga <miko09@poczta.onet.pl>
2025-06-25 16:31:19 +0200

    aktualizacja składniowa

--- Projekty/Python_9/weather_check.py
+++ Projekty/Python_9/weather_check.py
@@ -26,19 +26,21 @@ while True:
         # Jeśli parsowanie się nie powiodło, data jest niepoprawna
         print("Niepoprawny format daty. Spróbuj ponownie.")
 
-# Sprawdzenie, czy dane są już w pliku
+# Sprawdzenie, czy prognoza dla miasta i daty jest już zapisana w pliku JSON
 city_info = file_handler[city, date]
 
 if city_info != "Data not found":
-    print("Wynik z pliku:", city_info)
+    # Jeśli dane są dostępne w pliku — wyświetlamy je
+    print(f"Dla miasta {city} w dniu {date}: {city_info}")
 else:
-    # Funkcja do pobrania danych z API – musisz ją jeszcze zdefiniować
+    # Jeśli danych nie ma — pobieramy je z API przez funkcję z utility
     rain_sum = check_rain_status(city, date)
-
-    # Zapisujemy nowy wynik
+    # Zapisujemy nowo pobraną prognozę do pliku JSON
     file_handler[city, date] = rain_sum
     file_handler.write_to_file()
-    print("Wynik z API:", rain_sum)
+    # Wyświetlamy wynik pobrany z API
+    print(f"Dla miasta {city} w dniu {date}: {rain_sum}")
+
 
 #Mozliwosc testow
 # geolocator = Nominatim(user_agent="mikola_smaga_app")
